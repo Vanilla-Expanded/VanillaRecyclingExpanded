2fcd07c1f44ec54356b000f4421b426923bda476
SargBjornson <sarg.auxiliar@gmail.com>
2023-12-18 13:17:26 +0100

    Trash compactor

---
+++ 1.4/Source/VanillaRecyclingExpanded/VanillaRecyclingExpanded/DefOfs/InternalDefOf.cs
@@ -0,0 +1,20 @@
+﻿
+using RimWorld;
+using Verse;
+
+
+namespace VanillaRecyclingExpanded
+{
+    [DefOf]
+    public static class InternalDefOf
+    {
+
+
+        public static SoundDef VRecyclingE_Compactor;
+
+        static InternalDefOf()
+        {
+            DefOfHelper.EnsureInitializedInCtor(typeof(InternalDefOf));
+        }
+    }
+}
---
+++ 1.4/Source/VanillaRecyclingExpanded/VanillaRecyclingExpanded/Harmony/HarmonyInstance.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Reflection;
+using Verse;
+
+namespace VanillaRecyclingExpanded
+{
+    //Setting the Harmony instance
+    [StaticConstructorOnStartup]
+    public class Main
+    {
+        static Main()
+        {
+            var harmony = new Harmony("com.VERecycling");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());       
+        }          
+    }
+}
