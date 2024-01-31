c3c7b1ec1798ed1cfd12d0693fb0552aa5326e75
juanosarg <31105173+juanosarg@users.noreply.github.com>
2024-01-31 12:56:57 +0100

    Assorted changes

---
+++ 1.4/Source/VanillaRecyclingExpanded/VanillaRecyclingExpanded/Building/Building_WasteCrate.cs
@@ -0,0 +1,39 @@
+﻿
+using RimWorld;
+using Verse;
+using Verse.Sound;
+
+using Verse.Noise;
+using UnityEngine;
+using System;
+
+namespace VanillaRecyclingExpanded
+{
+     public class Building_WasteCrate : Building
+     {
+       
+
+        
+
+
+
+
+
+       
+         public override void Draw()
+         {
+
+             base.Draw();
+
+             
+             Vector3 drawPos2 = DrawPos;
+             drawPos2.y -= 3f / 148f;
+             def.building.wastepackAtomizerWindowGraphic.Graphic.Draw(drawPos2, base.Rotation, this);
+
+
+
+         }
+
+
+     }
+}
