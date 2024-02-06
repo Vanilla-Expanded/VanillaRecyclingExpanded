1d18feba6e6562a3c2d5b886e638b24c94d0dfd1
juanosarg <31105173+juanosarg@users.noreply.github.com>
2024-02-06 10:18:49 +0100

    v0.103

---
+++ 1.4/Source/VanillaRecyclingExpanded/VanillaRecyclingExpanded/PlaceWorkers/PlaceWorker_WasteCrate.cs
@@ -0,0 +1,14 @@
+﻿
+using RimWorld;
+using UnityEngine;
+using Verse;
+namespace VanillaRecyclingExpanded
+{
+    public class PlaceWorker_WasteCrate : PlaceWorker
+    {
+        public override void DrawGhost(ThingDef def, IntVec3 loc, Rot4 rot, Color ghostCol, Thing thing = null)
+        {
+            GhostUtility.GhostGraphicFor(GraphicDatabase.Get<Graphic_Single>(def.building.wastepackAtomizerBottomGraphic.texPath, ShaderDatabase.Cutout, def.building.wastepackAtomizerBottomGraphic.drawSize, Color.white), def, ghostCol).DrawFromDef(GenThing.TrueCenter(loc, rot, def.Size, AltitudeLayer.MetaOverlays.AltitudeFor()), rot, def);
+        }
+    }
+}
