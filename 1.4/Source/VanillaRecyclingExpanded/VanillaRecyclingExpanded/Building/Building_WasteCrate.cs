2699f3c40068d58f3ed5f85404dcea2a05da29e6
juanosarg <31105173+juanosarg@users.noreply.github.com>
2024-02-06 08:57:13 +0100

    v0.103

--- 1.4/Source/VanillaRecyclingExpanded/VanillaRecyclingExpanded/Building/Building_WasteCrate.cs
+++ 1.4/Source/VanillaRecyclingExpanded/VanillaRecyclingExpanded/Building/Building_WasteCrate.cs
@@ -25,10 +25,12 @@ namespace VanillaRecyclingExpanded
 
              base.Draw();
 
-             
-             Vector3 drawPos = DrawPos;
-            drawPos.y += 6;
-            def.building.wastepackAtomizerWindowGraphic.Graphic.DrawFromDef(drawPos, base.Rotation,null);
+            Vector3 drawPos = DrawPos;
+            drawPos.y += 5;
+            def.building.wastepackAtomizerBottomGraphic.Graphic.Draw(drawPos, base.Rotation, this);
+            Vector3 drawPos2 = DrawPos;
+            drawPos2.y += 6;
+            def.building.wastepackAtomizerWindowGraphic.Graphic.DrawFromDef(drawPos2, base.Rotation,null);
 
 
 
