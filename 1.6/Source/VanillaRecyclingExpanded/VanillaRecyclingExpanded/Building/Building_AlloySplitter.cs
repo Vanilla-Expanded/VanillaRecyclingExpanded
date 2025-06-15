aac09b5f68e5db06690dfd2b59843ccc6f535604
juanosarg <31105173+juanosarg@users.noreply.github.com>
2025-06-15 18:41:45 +0200

    v3.0

---
+++ 1.6/Source/VanillaRecyclingExpanded/VanillaRecyclingExpanded/Building/Building_AlloySplitter.cs
@@ -0,0 +1,78 @@
+﻿
+using RimWorld;
+using Verse;
+using Verse.Sound;
+
+using Verse.Noise;
+using UnityEngine;
+using System;
+
+namespace VanillaRecyclingExpanded
+{
+    public class Building_AlloySplitter : Building
+    {
+        private CompSuperSimpleProcessor comp;
+        public int tickCounter =0;
+        public const int interval = 600;
+        private Effecter operatingEffecter;
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+   
+            Scribe_Values.Look(ref tickCounter, "tickCounter");
+
+        }
+
+
+
+        public CompSuperSimpleProcessor Comp
+        {
+            get
+            {
+                if (comp == null)
+                {
+                    comp = GetComp<CompSuperSimpleProcessor>();
+                }
+                return comp;
+            }
+        }
+
+
+        protected override void Tick()
+        {
+            base.Tick();
+
+            if (Comp?.Working != true)
+            {
+                operatingEffecter?.Cleanup();
+                operatingEffecter = null;
+                return;
+            }
+            if (operatingEffecter == null)
+            {
+                operatingEffecter = def.building.wastepackAtomizerOperationEffecter.Spawn();
+                operatingEffecter.Trigger(this, new TargetInfo(InteractionCell, base.Map));
+            }
+            operatingEffecter.EffectTick(this, new TargetInfo(InteractionCell, base.Map));
+        }
+
+        protected override void DrawAt(Vector3 drawLoc, bool flip = false)
+        {
+           
+            base.DrawAt(drawLoc, flip);
+
+            Vector3 drawPos = DrawPos;
+            drawPos.y -= 0.08108108f;
+            def.building.wastepackAtomizerBottomGraphic.Graphic.Draw(drawPos, base.Rotation, this);
+            Vector3 drawPos2 = DrawPos;
+            drawPos2.y -= 3f / 148f;
+            def.building.wastepackAtomizerWindowGraphic.Graphic.Draw(drawPos2, base.Rotation, this);
+
+   
+
+        }
+
+      
+    }
+}
---
+++ 1.6/Source/VanillaRecyclingExpanded/VanillaRecyclingExpanded/Comps/CompToxGasOnDestroyed.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+using RimWorld;
+using UnityEngine;
+using Verse;
+namespace VanillaRecyclingExpanded
+{
+    public class CompToxGasOnDestroyed : ThingComp
+    {
+        public override void PostDestroy(DestroyMode mode, Map previousMap)
+        {
+            if (parent.PositionHeld != IntVec3.Invalid && previousMap!=null)
+            {
+                GasUtility.AddGas(parent.PositionHeld, previousMap, GasType.RotStink, 10);
+               
+            }
+             base.PostDestroy(mode, previousMap);
+        }
+    }
+}
