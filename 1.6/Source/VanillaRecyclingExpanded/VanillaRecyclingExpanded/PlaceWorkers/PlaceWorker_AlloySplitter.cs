851e5014ef01bc53d9db3c2b91c2ee663281474a
juanosarg <31105173+juanosarg@users.noreply.github.com>
2026-03-21 16:53:55 +0100

    v3.01

---
+++ 1.6/Source/VanillaRecyclingExpanded/VanillaRecyclingExpanded/PlaceWorkers/PlaceWorker_AlloySplitter.cs
@@ -0,0 +1,43 @@
+﻿
+using RimWorld;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+namespace VanillaRecyclingExpanded
+{
+    public class PlaceWorker_AlloySplitter : PlaceWorker
+    {
+        public override AcceptanceReport AllowsPlacing(BuildableDef checkingDef, IntVec3 loc, Rot4 rot, Map map, Thing thingToIgnore = null, Thing thingToPlace = null)
+        {
+            List<IntVec3> outputSlots = new List<IntVec3>() { loc + new IntVec3(1,0,-1).RotatedBy(rot),
+            loc + new IntVec3(0,0,-1).RotatedBy(rot),loc + new IntVec3(-1,0,-1).RotatedBy(rot)};
+
+            foreach(IntVec3 cell in outputSlots)
+            {
+                if (cell.InBounds(map))
+                {
+                    foreach (Thing item in map.thingGrid.ThingsListAtFast(cell))
+                    {
+                        Building itemBuilding = item as Building;
+                        if(itemBuilding!=null && itemBuilding.def.building.isEdifice)
+                        {
+                            return new AcceptanceReport("VRecyclingE_AlloySplitterOutputBlocked".Translate());
+
+                        }
+                    }
+                }
+                else{
+                    return new AcceptanceReport("VRecyclingE_AlloySplitterOutOfBounds".Translate());
+                }
+                
+                
+                  
+
+            }
+
+
+            return true;
+        }
+    }
+}
